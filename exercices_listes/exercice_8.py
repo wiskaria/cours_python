32b149f4996bf93aa656b89b28dfab3283f8dcf6
mounir-laity <mounirlaity@gmail.com>
2022-02-16 13:38:21 +0100

    added Robert's method for exo 8 of lists

--- exercices_listes/exercice_8.py
+++ exercices_listes/exercice_8.py
@@ -2,3 +2,9 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 for i in range(len(my_list) - 1):
     print(my_list[i : i + 2])
+
+
+# from statistics import mode
+
+# liste2 = [17, 38, 10, 25, 72, 10, 38, 10, 0, 0, 0, 1, 0, 0]
+# print(mode(liste2))
