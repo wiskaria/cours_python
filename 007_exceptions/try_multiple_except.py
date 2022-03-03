768e4defde321107db8014df726b7693cd33ea22
mounir-laity <mounirlaity@gmail.com>
2022-03-03 13:58:34 +0100

    updated try multiple except

---
+++ 007_exceptions/try_multiple_except.py
@@ -0,0 +1,10 @@
+numerateur = input("Entrez le numérateur : ")
+denominateur = input("Entrez le dénominateur : ")
+try:
+    numerateur = int(numerateur)
+    denominateur = int(denominateur)
+    result = numerateur / denominateur
+except ValueError:
+    print("Désolé la valeur saisie n'est pas un nombre.")
+except ZeroDivisionError:
+    print("On ne peut pas diviser par zéro !")
